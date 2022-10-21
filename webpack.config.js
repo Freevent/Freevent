d6a3ef1d47ce9a3baa57acb1bbcd9385329df770
kcuevas <cuev73@live.com>
2022-10-21 19:54:27 -0400

    initial file structure set up and webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+      publicPath: '/'
+    },
+    hot: true,
+    compress: true,
+    port: 8080,
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+  },
+  plugins: [new HtmlWebpackPlugin({template: './client/index.html'})],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      }
+    ]
+  }
+};
