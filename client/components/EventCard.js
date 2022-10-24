ed5d8ad113d84f3bac969d45bc5aba2ab90258ba
Anna Larouche <annalarouche@Annas-MacBook-Air.local>
2022-10-24 12:57:55 -0700

    styling, front and backend talking

--- client/components/EventCard.js
+++ client/components/EventCard.js
@@ -2,14 +2,21 @@ import React, { useState } from 'react';
 import Button from './Button';
 
 const EventCard = (props) => {
-    const { eventName, summary, date, location } = props.event
+    console.log(props)
+    const {
+        name,
+        start_date,
+        summary,
+        address
+    } = props.event
+    const date = start_date.slice(0, 10)
     return (
         <div className='eventCard'>
             <div className='info'>
-                <span>Who:</span> {eventName}
+                <span>Who:</span> {name}
                 <span>What:</span> {summary}
                 <span>When:</span> {date}
-                <span>Where:</span> {location}
+                <span>Where:</span> {address}
             </div>
             <div className='buttonWrapper'>
                 <Button />
